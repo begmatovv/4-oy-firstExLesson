481329e036a5585418d39cf4d9797c1eacbeea04
begmatovazizbek <begmatovazizbek119@gmail.com>
2023-07-28 09:44:53 +0500

    javascript beginner lessons

---
+++ main.js
@@ -0,0 +1,70 @@
+// 1-MISOL
+
+// const yosh = prompt(`yoshingizni kiriting`)
+// const yoshLimit = 50
+
+// if(yosh > 50){
+//     console.log(`Siz kattasiz`);
+// }else{
+//     console.log(`siz yoshsiz`);
+// }
+
+// 2-MISOL
+
+// const narx = prompt(`somsani narxini kiriting`)
+// if(narx > 3000){
+//     console.log(`qimmat`);
+// }else{
+//     console.log(`arzon`);
+// }
+
+// 3-MISOL
+
+// const parol = prompt(`parolni kiriting`)
+// const password = "sarimsoqpiyoz"
+// if(parol == password){
+//     console.log(`Assalomu alaykum`);
+// }else{
+//     console.log(`xatolik yuz berdi`);
+// }
+
+// 4-MISOL
+
+// const yosh = prompt(`yoshingizni kiriting`);
+// if (yosh > 0 && yosh <= 30) {
+//   console.log(`siz yoshsiz`);
+// } else if (yosh > 30 && yosh <= 60) {
+//   console.log(`siz katasiz`);
+// } else {
+//   console.log(`siz qarisiz`);
+// }
+
+// 5-MISOL
+// const ism = prompt(`Ismingizni kiriting`)
+
+// if(ism == "Ali"){
+//     console.log(`Salom og'a`);
+// }else if(ism == "Ahmad"){
+//     console.log(`what's up bro`)
+// }else{
+//     console.log(`uzur tanimadim`);
+// }
+
+// 6-7-MISOL????
+// const oylik = Number(prompt(`Oylik maoshingizni kiriting`))
+// let soliq = confirm(` soliq bn hisoblansinmi`)
+
+// let tartib = "birinchi";
+// let tartib2 = "ikkinchi";
+// let result = tartib2;
+// tartib2=tartib
+// tartib=result
+// console.log(tartib);
+// console.log(tartib2);
+
+// let ism = prompt("ismingizni kiriting");
+// if (ism.includes(`bek`) || ism.includes(`jon`)) {
+//   console.log(`Salom ${ism}`);
+// } else {
+//   console.log(`Salom ${ism}bek`);
+// }
